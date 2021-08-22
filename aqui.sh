e1b5afcde68ba0d844c9495e769943779211e40a
diego <dih.cvel@gmail.com>
2021-08-22 11:15:42 -0300

    config ala o linux

--- aqui.sh
+++ aqui.sh
@@ -1,8 +1,8 @@
-cd JCup
-del /f parser.java 
-del /f sym.java
+#!/bin/bash
+cd src/JCup
+sudo rm parser.java && rm sym.java
 java -jar java-cup-11b.jar parser.cup
+
 cd ../JFlex
-del /f Lexer.java
+sudo rm Lexer.java
 java -jar jflex-full-1.8.2.jar lexer.flex
-exit
--- src/.bat
+++
@@ -1,8 +0,0 @@
-cd JCup
-del /f parser.java 
-del /f sym.java
-java -jar java-cup-11b.jar parser.cup
-cd ../JFlex
-del /f Lexer.java
-java -jar jflex-full-1.8.2.jar lexer.flex
-exit
