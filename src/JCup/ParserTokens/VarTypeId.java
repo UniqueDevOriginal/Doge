77e5be29aae240919ded037d5aee3e13f5821f16
Eb3r-r00t <eberson_taynan@hotmail.com>
2021-08-29 23:59:34 -0300

    ajustes de elegancia

--- src/JCup/ParserTokens/VarTypeId.java
+++ src/JCup/ParserTokens/VarTypeId.java
@@ -1,26 +1,23 @@
 package JCup.ParserTokens;
 
-
 public class VarTypeId extends Var {
-    TypeId child1;
-    Id     child2;
+  TypeId child1;
+  Id child2;
 
   public VarTypeId(TypeId type, Id id) {
     child1 = type;
     child2 = id;
   }
 
-@Override
-public
-void print(String prefix, boolean isTail) {
-  
-  String type = "Var";
-	System.out.println(prefix + (isTail ?  "|-- " : "|-- ") + type);   
-	child1.print(prefix + (isTail ? "|   " : "|   "), false);
-  System.out.println(prefix + "|   " + (isTail ?  "| " : "| "));
-  child2.print(prefix + (isTail ? "|   " : "|   "), true);
-  
-    
-}
+  @Override
+  public void print(String prefix, boolean isTail) {
+
+    String type = "Var";
+    System.out.println(prefix + (isTail ? "|-- " : "|-- ") + type);
+    child1.print(prefix + (isTail ? "|   " : "|   "), false);
+    System.out.println(prefix + "|   " + (isTail ? "|   " : "|   "));
+    child2.print(prefix + (isTail ? "|   " : "|   "), true);
+
+  }
 
 }
